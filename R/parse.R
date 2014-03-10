19b49bbb72323a7f393ed71d86a4536043cbfc9b
brodieG <brodieG@users.noreply.github.com>
2014-03-09 22:00:24 -0400

    continuing to parse comments, work in progress

--- R/parse.R
+++ R/parse.R
@@ -40,9 +40,14 @@
 # not if we process it all in one go?)
 
 
-#' Searches Through Generation Until it Finds Top Level
+#' Searches Through Generations Until it Finds Top Level
 #'
-#' Returns the id of the ancestor that is just before \code{`id==0`}
+#' Returns the id of the ancestor that is just before \code{`top.level`},
+#' or \code{`top.level`} if the parent already is \code{`top.level`}. The
+#' idea is to reduce the set of parents for all elements to just the top
+#' level parents as this allows us to split the parse data into sections,
+#' including the calls that were direct children of top level, as well as
+#' the children to those sections.
 #' 
 #' @param ids integer the ids to look through
 #' @param par.ids integer the parenthood relationships
@@ -57,6 +62,11 @@ top_level_parse_parents <- function(ids, par.ids, top.level=0L) {
     stop("Argument `par.ids` contains ids not in `ids`.")
   if(!is.integer(top.level) && !identical(length(top.level), 1L))
     stop("Argument `top.level` must be a one length integer")
+  if(identical(top.level, 0L)) {
+    par.ids <- pmax(0L, par.ids)
+  } else if (any(par.ids) < 0) {
+    stop("Argument `par.ids` contains values less than zero, but that is only allowed when `top.level` == 0L")
+  }
   ancestry_descend <- function(par.id) {
     par.id <- abs(par.id)
     if(identical(par.id, top.level)) return(par.id)
@@ -67,78 +77,156 @@ top_level_parse_parents <- function(ids, par.ids, top.level=0L) {
   vapply(par.ids, ancestry_descend, integer(1L))
 }
 
+#' Assign Comments From Parse Data to Expression Elements
+#' 
+#' Based on parse data from \code{`\link{getParseData}`}, figures
+#' out what comments belong to what expression.  If a comment is
+#' on the same line as an expression, the comment is assigned to that
+#' expression (or whatever the nearest expression is on that line if
+#' there is more than one).  If a comment is on it's own line,
+#' then the match is done to the next expression.
+#' 
+#' The expectation is that only "top level" expressions will
+#' be submitted as part of `comment.dat` (i.e. they all have
+#' the same parent, they don't strictly have to be top.level).
+#' 
+#' @param expr and expression
+#' @param comment.dat a data frame derived from \code{`\link{getParseData}`}
+#' @return an expression with comments attached as attributes to each 
+#'   expression component
 
-parse_data_assign <- function(expr, parse.data, top.level=0L) {
-  # if(!is.language())  # NOT CLEAR IF WE NEED / CAN TEST EXPRESSION
-  if(!is.data.frame(parse.data)) stop("Argument `parse.data` must be a data frame.")
-  if(!identical(names(parse.data), c("line1", "col1", "line2", "col2", "id", "parent", "token",  "terminal", "text")))
-    stop("Argument `parse.data` does not have the expected column names")
-  if(!identical(cat(deparse(unname(vapply(dat, class, "")))), c("integer", "integer", "integer", "integer", "integer", "integer",  "character", "logical", "character")))
-    stop("Argument `parse.data` does not have the expected column data types")
-  #  MOVE IN PARSE DATA GENERATION STEP INTO HERE
-  if(identical(top.level, 0L)) parse.data <- transform(parse.data, parent=ifelse(parent < 0, 0L, parent))
+comments_assign <- function(expr, comment.dat) {
+  if(!identical(length(unique(comment.dat$parent)), 1L))
+    stop("Logic Error: there were multiple parent ids in argument `comment.dat`; this should not happen")
 
-  comments_assign <- function(expr, comment.dat) {
-    # what comments are on same line as something else
+  # what comments are on same line as something else
 
-    comm.comm <- subset(comment.dat, token=="COMMENT")
-    comm.expr <- subset(comment.dat, token=="expr")
-
-    comment.expr <- transform(  # identify whether a token is the first or last on it's line
-      comment.expr, 
-      first.last.on.line=ave(
-        col1, line1, 
-        FUN=function(x) if(identical(length(x), 1L)) 3L else ifelse(x == max(x), 2L, ifelse(x == min(x), 1L, 0L))
-    ) )
-    # For each comment on a line that also has an expression, find the expression 
-    # that is also on that line
-
-    comm.comm <- transform(comm.comm, assign.to.prev=match(line1, comm.expr$line2))
-    comm.comm$match <- with(comm.expr,  {
-      last.or.only <- first.last.on.line %in% 2L:3L
-      first.or.only <- first.last.on.line %in% c(1L, 3L)
-      id[last.or.only][match(comm.comm$assign.to.prev, line1[last.or.only]]))
-    } )  
-    # For each comment on its own line, find the expression that follows it
-    
-    transform(comm.comm, assign.to.next=vapply(line1, function(x) min(comm.expr$line1[comm.expr$line1 > x]), integer(1L)))
-    comm.comm$match <- with(comm.expr, 
-      ifelse(
-        is.na(comm.comm$match), 
-        id[first.or.only][match(comm.comm$assign.to.next, line1[first.or.only])],
-        comm.comm$match
-    ) )
-    # Assign comments to matching expression in attributes
-    
-    with(
-      subset(comm.comm, !is.na(match)), {
-        for(i in seq_along(match)) {
-          expr.pos <- which(comm.expr == match[[i]])
-          if(!identical(length(expr.pos), 1L)) stop("Logic Error; contact maintener.")
-          attr(expr[[expr.pos]], "comment") <- c(attr(expr[[expr.pos]], "comment"), text[[i]])
-    } } )
-    expr
+  comm.comm <- subset(comment.dat, token=="COMMENT")
+  comm.expr <- subset(comment.dat, token=="expr")
+  comm.notcomm <- subset(comment.dat, (token %in% c("'['", "'{'", "expr")) | (token == "'('" & 1:nrow(comment.dat) == 1L))
+  
+  if(!identical(nrow(comm.notcomm), length(expr))) {
+    stop("Argument `expr` length cannot be matched with values in `comment.dat`")
   }
+
+  comm.expr <- transform(  # identify whether a token is the first or last on it's line
+    comm.expr, 
+    first.last.on.line=ave(
+      col1, line1, 
+      FUN=function(x) if(identical(length(x), 1L)) 3L else ifelse(x == max(x), 2L, ifelse(x == min(x), 1L, 0L))
+  ) )
+  # For each comment on a line that also has an expression, find the expression 
+  # that is also on that line
+
+  comm.comm <- transform(comm.comm, assign.to.prev=comm.expr$line2[match(line1, comm.expr$line2)])
+  comm.comm$match <- with(comm.expr,  {
+    last.or.only <- first.last.on.line %in% 2L:3L
+    id[last.or.only][match(comm.comm$assign.to.prev, line1[last.or.only])]
+  } )  
+  # For each comment on its own line, find the expression that follows it
+  
+  first.or.only <- comm.expr$first.last.on.line %in% c(1L, 3L)
+  comm.comm$assign.to.next <- vapply(
+    comm.comm$line1, 
+    function(x) if(any(idx <- (comm.expr$line1 > x))) min(comm.expr$line1[idx]) else NA_integer_, 
+    integer(1L)
+  )
+  comm.comm$match <- ifelse(
+    is.na(comm.comm$match), 
+    comm.expr$id[first.or.only][match(comm.comm$assign.to.next, comm.expr$line1[first.or.only])],
+    comm.comm$match
+  )
+  # Assign comments to matching expression in attributes
+  
+  for(i in seq_along(comm.comm$match)) {
+    if(is.na(comm.comm$match[[i]])) next
+    expr.pos <- which(comm.notcomm$id == comm.comm$match[[i]])
+    if(!identical(length(expr.pos), 1L)) stop("Logic Error; contact maintainer.")
+    attr(expr[[expr.pos]], "comment") <- c(attr(expr[[expr.pos]], "comment"), comm.comm$text[[i]])
+  }
+  expr
+}
+#' Recursively Descends Through a Parsed Expression and Assigns Comments
+#' 
+#' In order to implement this we had to make several assumptions about the
+#' behaviour of \code{`\link{getParseData}`}.  In particular:
+#' \itemize{
+#'   \item Top level comments show up with negative ids, but are top level
+#'     for all intents and purposes
+#'   \item All content tokens (i.e. anything other than brackets, commas,
+#'     etc.) are contained inside an \code{`expr`}, even if the only thing the
+#'     `expr` contains is a simple constant.
+#'   \item Comments are not content tokens and can exist on the top level
+#'     without being wrapped in an \code{`expr`}
+#'   \item The only tokens that count as elements in an expression are
+#'     opening brackets and \code{`expr`}; this assumption is necessary
+#'     to allow mapping the parsed data back to the expression.  What
+#'     confuses the issue a bit is that operators (e.g. \code{`:`} or
+#'     \code{`%in%`}, etc.) show up at the top level, but you can actually
+#'     ignore them.  Also, parantheses should only be kept if they are the
+#'     topmost item, as otherwise they are part of a function call and
+#'     should be ignored.
+#' }
+#' Note that as a result of this trial and error interpretation of 
+#' \code{`\link{getParseData}`} it is likely that comment parsing is
+#' not 100% robust.
+#' 
+#' @seealso comments_assign, getParseData, parse
+#' @param an expression produced by \code{`\link{parse}`}
+#' @return an expression with comments retrieved from the parse attached
+#'   to the appropriate sub-expressions/calls as a "comment" \code{`\link{attr}`}
+
+parse_data_assign <- function(expr) {
+  if(!is.expression(expr)) stop("Argument `expr` must be an expression")
+  parse.dat <- getParseData(expr)
+  if(is.null(parse.dat)) stop("Argument `expr` did not contain any parse data")
+  if(!is.data.frame(parse.dat)) stop("Argument `expr` produced parse data that is not a data frame")
+  if(!identical(names(parse.dat), c("line1", "col1", "line2", "col2", "id", "parent", "token",  "terminal", "text")))
+    stop("Argument `expr` produced parse data with unexpected column names")
+  if(!identical(unname(vapply(dat, class, "")), c("integer", "integer", "integer", "integer", "integer", "integer",  "character", "logical", "character")))
+    stop("Argument `expr` produced data with unexpected column data types")
+  parse.dat <- transform(parse.dat, parent=ifelse(parent < 0, 0L, parent))
+
   prsdat_recurse <- function(expr, parse.dat, top.level) {
     
-    par.ids <- with(parse.data, top_level_parse_parents(id, parent))
-    parse.data.split <- split(parse.dat, par.ids)
-    prsdat.par <- parse.data.split[[as.character(parent)]]
-    if(!identical(sort(unique(prsdat.par$token)), c("COMMENT", "expr")))
-      stop("Logic Error, unexpected token values; contact maintainer.")
-    prsdat.children <- parse.data.split[names(parse.data.split) != as.character(parent)]
+    browser()
+    par.ids <- with(parse.dat, top_level_parse_parents(id, parent, top.level))
+    parse.dat.split <- split(parse.dat, par.ids)
+    prsdat.par <- parse.dat.split[[as.character(top.level)]]
+    # if(!identical(sort(unique(prsdat.par$token)), c("COMMENT", "expr", "")))
+    #   stop("Logic Error, unexpected token values; contact maintainer.")
+    prsdat.children <- parse.dat.split[names(parse.dat.split) != as.character(top.level)]
 
-    if(!identical(length(expr), length(prsdat.children))) stop("Logic Error: mismatch between parse data and expression")
-
+    if(!identical(length(expr), length(prsdat.children))) {
+      browser()
+      stop("Logic Error: mismatch between parse data and expression")
+    }
+    line.dat <- vapply(prsdat.children, function(x) c(max=max(x$line2), min=min(x$line1)), c(max=0L, min=0L))
+    col.dat <- vapply(
+      seq_along(prsdat.children), 
+      function(i) c(
+        max=max(subset(prsdat.children[[i]], line2==line.dat["max", i])$col2), 
+        min=min(subset(prsdat.children[[i]], line1==line.dat["min", i])$col1)
+      ),
+      c(max=0L, min=0L)
+    )
+    if(
+      any(head(line.dat["max", ], -1L) > tail(line.dat["min", ], -1L)) ||
+      any(
+        head(line.dat["max", ], -1L) == tail(line.dat["min", ], -1L) &
+        head(col.dat["max", ], -1L) >= tail(col.dat["min", ], -1L))
+    ) {
+      stop("Logic Error: expression parse data overlapping; contact maintainer")
+    }
     expr <- comments_assign(expr, prsdat.par)
     
-    # NEED TO GUARANTEE expr AND prsdat.children ARE SAME ORDER!!!
-
     for(i in seq_along(prsdat.children)) {
       expr[[i]] <- Recall(expr[[i]], prsdat.children[[i]], as.integer(names(prsdat.children)[[i]]))
     }
     expr
-} }
+  }
+  prsdat_recurse(expr, parse.dat, top.level=0L)
+}
 
 
 
--- tests/testthat/testthat.parse.R
+++ tests/testthat/testthat.parse.R
@@ -3,23 +3,36 @@ library(testor)
 
 prs <- parse("tests/testthat/parse.data/file1.R")
 dat <- getParseData(prs)
+dat$parent <- pmax(0L, dat$parent)
 
-expect_equal(
+expect_equal(info="Identified top level parents?",
   par.ids <- with(dat, testor:::top_level_parse_parents(id, parent)),
-  c(12L, 0L, 12L, 12L, 12L, 12L, 12L, 45L, 45L, 45L, 0L, 45L, 45L,  45L, 45L, 45L, 45L, 45L, 45L, 45L, 45L, 45L, 45L, 45L, 112L,  0L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L,  112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L,  112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L,  112L, 112L, 112L, 112L, 112L, 112L, 0L, 128L, 128L, 128L, 128L,  128L, 128L, 128L, 128L, 128L, 128L, 147L, 0L, 147L, 147L, 147L,  147L, 147L, 147L, 0L, 159L, 159L, 159L, 159L, 159L, 159L, 0L)
+  c(0L, 0L, 12L, 12L, 12L, 12L, 12L, 0L, 0L, 0L, 0L, 45L, 45L,  45L, 45L, 45L, 45L, 45L, 45L, 45L, 45L, 45L, 45L, 45L, 0L, 0L,  112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L,  112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L,  112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L, 112L,  112L, 112L, 112L, 112L, 112L, 0L, 128L, 128L, 128L, 128L, 128L,  128L, 128L, 128L, 128L, 128L, 0L, 0L, 147L, 147L, 147L, 147L,  147L, 147L, 0L, 159L, 159L, 159L, 159L, 159L, 159L, 0L)
+)
+dat.split <- split(dat, par.ids)
+
+expect_equal(info="Did we assign comments correctly to topmost level?",
+  lapply(testor:::comments_assign(prs, dat.split$`0`), attr, "comment"),
+  list("# This is an early comment", c("# multi", "# line", "# comment",  "# and another!"), NULL, NULL, "# and this comment belongs to whom?",      "# and I?")
 )
 
-dat.split <- split(dat, par.ids)
-expect_equal(
+expect_equal(info="Identified sub-level top level parents correctly",
   par.ids.2 <- with(dat.split$`112`, testor:::top_level_parse_parents(id, parent, 112L)), 
-  c(112L, 54L, 112L, 112L, 57L, 112L, 112L, 112L, 108L, 108L, 108L,  108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L,  108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L,  108L, 108L, 108L, 108L, 108L, 112L)
+  c(54L, 112L, 112L, 57L, 112L, 112L, 112L, 108L, 108L, 108L, 108L,  108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L,  108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L, 108L,  108L, 108L, 108L, 108L, 112L)
 )
 dat.split.2 <- split(dat.split$`112`, par.ids.2)
 
-# Reset negative parents to zero due to the odd treatment of top level comments
+expect_equal(info="No comments here so no changes should occur",
+  testor:::comments_assign(prs[[3]], dat.split.2$`112`),
+  prs[[3]]
+)
+expect_equal(info="Parent relationships in `testor_sect` piece.",
+  par.ids.3 <- with(dat.split.2$`108`, testor:::top_level_parse_parents(id, parent, 108L)),
+  c(108L, 108L, 108L, 76L, 76L, 76L, 76L, 76L, 76L, 76L, 76L, 76L,  108L, 108L, 108L, 100L, 100L, 100L, 100L, 100L, 100L, 100L, 100L,  100L, 100L, 100L, 100L, 100L, 108L, 108L)
+)
+expect_equal(info="Comments in `testor_sect` body assigned correctly",
+  testor:::comments_assign(prs[[3]][[3]], split(dat.split.2$`108`, par.ids.3)$`108`),
+  list(NULL, c("# test that were not crazy", "# TRUE hopefully" ), "# Still not crazy")
+)
 
-dat.new <- transform(dat, parent=ifelse(parent < 0, 0L, parent))
-par.ids <- with(dat.new, testor:::top_level_parse_parents(id, parent))
-dat.new.split <- split(dat.new, par.ids)
-par.ids.2 <- with(dat.new.split$`112`, testor:::top_level_parse_parents(id, parent, 112L))
-dat.new.split.2 <- split(dat.new.split$`112`, par.ids.2)
+
