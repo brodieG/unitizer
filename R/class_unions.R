76df3bc718440dc5841270df981a72605a0857a4
brodieG <brodieG@users.noreply.github.com>
2014-08-03 15:52:23 -0400

    added package_version oldClass to address validObject issues

--- R/class_unions.R
+++ R/class_unions.R
@@ -3,4 +3,5 @@ setClassUnion("characterOrNULL", c("character", "NULL"))
 setClassUnion("environmentOrNULL", c("environment", "NULL"))
 setClassUnion("subIndex", c("character", "logical", "numeric", "missing"))
 setOldClass("file")
+setOldClass(c('package_version', 'numeric_version'))
 # setClassUnion("fileOrNULL", c("file", "NULL"))  # removed due to conflicts with RJSONIO
