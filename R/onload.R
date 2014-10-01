69086f6711616ba4b608a267dd1a87f52c8f00ed
brodieG <brodieG@users.noreply.github.com>
2014-10-01 15:18:34 -0400

    added option to pretend we are in non-interactive mode

--- R/onload.R
+++ R/onload.R
@@ -3,11 +3,13 @@
   options(unitizer.disable.capt=FALSE)        # Will prevent capture
   options(unitizer.test.out.lines=c(50, 15))  # How many lines to display when showing test values, or truncate to if exceeds
   options(unitizer.test.msg.lines=c(10, 3))   # How many lines to display when showing test errors, or truncate to if exceeds
+  options(unitizer.non.interactive=FALSE)     # Pretend we're running in non-interactive mode
 }
 .onUnload <- function(libpath) {
   options(unitizer.show.output=NULL) 
   options(unitizer.disable.capt=NULL)  
   options(unitizer.test.out.lines=NULL)
   options(unitizer.test.msg.lines=NULL)  
+  options(unitizer.non.interactive=NULL)
 }
 
