b8aabfd19d3070d31554f1c345392ce02324ac97
brodieG <brodieG@users.noreply.github.com>
2014-10-01 20:31:37 -0400

    adding some more interactive tests
    Related to issue #6

---
+++ tests/interactive/unitizer/ignoreerror.R
@@ -0,0 +1,6 @@
+TRUE
+xx <- booboo # error on assign
+xx
+yy <- boo  # this causes an error, but should only be shown when there are non-ignored events that affect unitizer
+FALSE
+6
---
+++ tests/interactive/unitizer/ignoreerror2.R
@@ -0,0 +1,7 @@
+TRUE
+xx <- booboo # error on assign
+xx
+yy <- boo    # this causes an error, but should only be shown when there are non-ignored events that affect unitizer
+FALSE
+6
+7
