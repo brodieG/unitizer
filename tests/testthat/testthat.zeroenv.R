9c293246584a1c4e11edb613c85666b0cfe124ce
brodieG <brodieG@users.noreply.github.com>
2014-10-09 21:44:49 -0400

    initial tests for unitizer_library

---
+++ tests/testthat/testthat.zeroenv.R
@@ -0,0 +1,27 @@
+library(testthat)
+
+try(detach("package:unitizer", unload=TRUE))
+try(detach("package:unitizerdummypkg1", unload=TRUE))
+try(detach("package:unitizerdummypkg2", unload=TRUE))
+
+library(unitizer)
+
+# Package added properly
+
+search.path <- search()
+unitizer:::unitizer_library(unitizerdummypkg1)
+expect_equal(new.pack <- setdiff(search(), search.path), "package:unitizerdummypkg1")
+expect_equal(new.pack, names(unitizer:::pack.env$objects.attached))
+
+# Package added only once
+
+unitizer:::unitizer_library(unitizerdummypkg1)
+expect_equal(new.pack <- setdiff(search(), search.path), "package:unitizerdummypkg1")
+expect_equal(new.pack, names(unitizer:::pack.env$objects.attached))
+
+# Don't add packages that are already in search
+
+library(unitizerdummypkg2)
+search.path <- search()
+unitizer:::unitizer_library(unitizerdummypkg2)
+expect_equal(new.pack <- setdiff(search(), search.path), character())
