87bc3ebecaab3659800684b21cb73609eba1ac50
brodieG <brodieG@users.noreply.github.com>
2014-10-11 20:26:52 -0400

    substantial test re-org, and is.loaded_package tests

--- tests/testthat/testthat.zeroenv.R
+++ tests/testthat/testthat.zeroenv.R
@@ -6,71 +6,75 @@ try(detach("package:unitizerdummypkg2", unload=TRUE))
 
 library(unitizer)
 
-# Package added properly
+test_that("Detecting packages", {
+  expect_true(unitizer:::is.loaded_package("package:unitizer"))
+  expect_error(unitizer:::is.loaded_package("unitizer"))
+  expect_true(unitizer:::is.loaded_package("package:stats"))
+  expect_error(unitizer:::is.loaded_package(1))
+  expect_error(unitizer:::is.loaded_package(letters))
+  expect_error(unitizer:::is.loaded_package("Autoloads"))
+} )
+test_that("Package added properly", {
+  search.path <- search()
+  unitizer:::unitizer_library(unitizerdummypkg1)
+  expect_equal(new.pack <- setdiff(search(), search.path), "package:unitizerdummypkg1")
+  expect_equal(new.pack, names(unitizer:::pack.env$objects.attached))  
 
-search.path <- search()
-unitizer:::unitizer_library(unitizerdummypkg1)
-expect_equal(new.pack <- setdiff(search(), search.path), "package:unitizerdummypkg1")
-expect_equal(new.pack, names(unitizer:::pack.env$objects.attached))
+  # And only once
 
-# Package added only once
+  unitizer:::unitizer_library(unitizerdummypkg1)
+  expect_equal(new.pack <- setdiff(search(), search.path), "package:unitizerdummypkg1")
+  expect_equal(new.pack, names(unitizer:::pack.env$objects.attached))
+} )
+test_that("Don't add if already in search path", {
+  library(unitizerdummypkg2)
+  search.path <- search()
+  unitizer:::unitizer_library(unitizerdummypkg2)
+  expect_equal(new.pack <- setdiff(search(), search.path), character())
 
-unitizer:::unitizer_library(unitizerdummypkg1)
-expect_equal(new.pack <- setdiff(search(), search.path), "package:unitizerdummypkg1")
-expect_equal(new.pack, names(unitizer:::pack.env$objects.attached))
+  # Require too
 
-# Don't add packages that are already in search
+  try(detach("package:unitizerdummypkg2", unload=TRUE))
+  search.path <- search()
+  unitizer:::unitizer_require(unitizerdummypkg2)
+  expect_equal(new.pack <- setdiff(search(), search.path), "package:unitizerdummypkg2")
+  expect_equal(
+    c("package:unitizerdummypkg1", "package:unitizerdummypkg2"), 
+    names(unitizer:::pack.env$objects.attached)
+  )
+} )
+test_that("Trim and Reset Search Path", {
+  try(detach("package:unitizer", unload=TRUE))
+  try(detach("package:unitizerdummypkg1", unload=TRUE))
+  try(detach("package:unitizerdummypkg2", unload=TRUE))
 
-library(unitizerdummypkg2)
-search.path <- search()
-unitizer:::unitizer_library(unitizerdummypkg2)
-expect_equal(new.pack <- setdiff(search(), search.path), character())
+  library(unitizer)
+  library(unitizerdummypkg1)  # at least one pack to explicitly remove
 
-# Check that require also works
+  search.pre <- search()
+  search.pre.objs <- lapply(search.pre, as.environment)
 
-try(detach("package:unitizerdummypkg2", unload=TRUE))
+  testthat::expect_identical(unitizer:::search_path_trim(), NULL)  # note this actually removes testthat from search path...
 
-unitizer:::unitizer_require(unitizerdummypkg2)
-search.path <- search()
-expect_equal(new.pack <- setdiff(search(), search.path), "package:unitizerdummypkg2")
-expect_equal(new.pack, names(unitizer:::pack.env$objects.attached))
-expect_equal(
-  c("package:unitizerdummypkg1", "package:unitizerdummypkg2"), 
-  names(unitizer:::pack.env$objects.attached)
-)
+  search.post <- search()
+  search.base <- c(
+    ".GlobalEnv", "package:unitizer", "package:stats", "package:graphics",  
+    "package:grDevices", "package:utils", "package:datasets", "package:methods",  
+    "Autoloads", "package:base"
+  )
+  testthat::expect_true(
+    identical(search.post, search.base) || 
+    identical(search.post, append(search.base, "tools:rstudio", 2L))
+  )
+  testthat::expect_true(
+    "package:unitizerdummypkg1" %in% 
+    vapply(unitizer:::pack.env$objects.detached, `[[`, "", "name")
+  )
+  unitizer:::search_path_restore()
+  expect_identical(search.pre, search())
 
-# Check search path trimming
+  # Can't do identical b/c environments are reloaded and as such are different
+  # but this should at least compare attributes and all that
 
-try(detach("package:unitizer", unload=TRUE))
-try(detach("package:unitizerdummypkg1", unload=TRUE))
-try(detach("package:unitizerdummypkg2", unload=TRUE))
-
-library(unitizer)
-library(unitizerdummypkg1)  # at least one pack to explicitly remove
-
-search.pre <- search()
-
-testthat::expect_identical(unitizer:::search_path_trim(), NULL)  # note this actually removes testthat from search path...
-
-search.post <- search()
-search.base <- c(
-  ".GlobalEnv", "package:unitizer", "package:stats", "package:graphics",  
-  "package:grDevices", "package:utils", "package:datasets", "package:methods",  
-  "Autoloads", "package:base"
-)
-testthat::expect_true(
-  identical(search.post, search.base) || 
-  identical(search.post, append(search.base, "tools:rstudio", 2L))
-)
-testthat::expect_true(
-  "package:unitizerdummypkg1" %in% 
-  vapply(unitizer:::pack.env$objects.detached, `[[`, "", "name")
-)
-
-# Reset search path
-
-unitizer:::search_path_restore()
-expect_identical(search.pre, search())
-
-
-
+  expect_equal(search.pre.objs, lapply(search(), as.environment))
+} )
