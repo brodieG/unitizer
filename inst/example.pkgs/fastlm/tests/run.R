1778cfad7117f4c650e65af5d054489a4d60cdb9
brodieG <brodieG@users.noreply.github.com>
2014-10-19 22:17:07 -0400

    moving example packages to inst/example.pkgs/

---
+++ inst/example.pkgs/fastlm/tests/run.R
@@ -0,0 +1,8 @@
+library(unitizer)
+
+# f <- file("/Volumes/PERSONAL/RDevBG/fastlm/out.txt")
+# vec <- c(getwd(), system.file(), list.files("./unitizer"))
+
+# writeLines(vec, f)
+
+unitize("unitizer/fastlm.R")
