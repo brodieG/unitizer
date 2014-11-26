86290153df0744198014325646d29fa37b437891
brodieG <brodieG@users.noreply.github.com>
2014-11-25 22:07:19 -0500

    fastlm updates for demo
    created multiple versions of fastlm, stripped them
    down to basics

--- inst/example.pkgs/fastlm/tests/run.R
+++
@@ -1,8 +0,0 @@
-library(unitizer)
-
-# f <- file("/Volumes/PERSONAL/RDevBG/fastlm/out.txt")
-# vec <- c(getwd(), system.file(), list.files("./unitizer"))
-
-# writeLines(vec, f)
-
-unitize("unitizer/fastlm.R")
