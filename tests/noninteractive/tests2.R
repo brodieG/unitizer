a93db1c26c9ae09c39caf67c2f41bc9c85c5ed15
brodieG <brodieG@users.noreply.github.com>
2014-12-04 21:09:16 -0500

    more R CMD check fixes

---
+++ tests/noninteractive/tests2.R
@@ -0,0 +1,10 @@
+1 + 1
+print("heya")
+stop("wow")
+matrix(runif(21), nrow=3)
+lm(x ~ y, data.frame(x=1:10, y=2 * (1:10)))
+warning("this is a warning\nthat\nspans\nmany\nlines")
+data.frame(
+  a=1:20,
+  b=letters[1:20]
+)
