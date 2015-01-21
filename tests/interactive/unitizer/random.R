b79b2d848aed6110169f2bc4d24f93b129a8fd4a
brodieG <brodieG@users.noreply.github.com>
2015-01-20 20:00:15 -0500

    test update

--- tests/interactive/unitizer/random.R
+++ tests/interactive/unitizer/random.R
@@ -2,9 +2,11 @@
 
 x <- 200
 y <- 30
-{
-  message(sample(1:5))
-  matrix(runif(1:30), ncol=5)
+unitizer_sect("blah", compare=unitizerItemTestsFuns(value=identical, output=all.equal),
+{{
+  #message(sample(1:5), appendLF = FALSE)
   warning(runif(1))
-}
+  if(runif(1) > .5) warning(sample(1:5))
+  matrix(runif(1:60), ncol=2)
+}})
 
