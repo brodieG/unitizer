c978b3f6bdb774c89bc976fdec2db3afb7f54d73
brodieG <brodieG@users.noreply.github.com>
2015-01-24 14:57:28 -0500

    'editFunNames' now prompts before acting, and...
    + better documentation
    + examples involve get_, set_

--- R/rename.R
+++ R/rename.R
@@ -1,24 +1,56 @@
-
-setGeneric("edit_fun_names", function(x, fun.name.old, fun.name.new, ...)
-  standardGeneric("edit_fun_names")
-)
 #' Edit Calls In Unitizer
 #'
 #' Used if you want to change the name of a function you have pre-existing
-#' tests for.
+#' tests for.  Useful if you want to change the function name, but not have to
+#' re-run and review all the tests since they will no longer match due to the
+#' function name change.
 #'
+#' @note this is a somewhat experimental function, so make sure you backup any
+#'   unitizers before you try to use it.
+#'
+#' @export
+#' @rdname editFunNames
 #' @param x a unitizer object
 #' @param fun.name.old the name of the function replace
 #' @param fun.name.new the new name of the function
 #' @param ... unused
 #' @return a untizer object with function names modifies
 
-setMethod("edit_fun_names", c("unitizer", "name", "name"),
-  function(x, fun.name.old, fun.name.new, ...) {
+setGeneric("editFunNames", function(x, fun.name.old, fun.name.new, ...)
+  standardGeneric("editFunNames")
+)
+#' @export
+#' @rdname editFunNames
+#' @param interactive.only logical(1L) set to FALSE if you want to allow this to
+#'   run in non-interactive mode, but warnings will be suppressed and will
+#'   proceed without prompting, obviously...
+#' @examples
+#' \donrun{
+#' untz <- get_unitizer("tests/unitizer/mytests.unitizer")
+#' untz.edited <- editFunNames(untz, quote(myFun), quote(my_fun))
+#' set_unitizer(untz.edited, "tests/unitizer/mytests.unitizer")
+#' }
+
+setMethod("editFunNames", c("unitizer", "name", "name"),
+  function(x, fun.name.old, fun.name.new, interactive.only=TRUE, ...) {
     warning(
       "This is an experimental function; make sure you backup any unitizers ",
-      "you edit with this"
+      "before you edit them", immediate.=TRUE
     )
+    if(!interactive() && interactive.only)
+      stop("Set interactive.only to FALSE to run in non-interactive mode")
+    i <- 0L
+    repeat {
+      ans <- readline("Do you wish to proceed ([Y]es/[N]o)? ")
+      if(tolower(ans) %in% c("y", "yes")) break
+      else if(tolower(ans) %in% c("n", "no")) {
+        message("Existing without edits")
+        return(x)
+      } else {
+        message("Invalid input, should be \"Y\" or \"N\"")
+      }
+      if((i <- i + 1L) > 2L) stop("You are not making sense; I give up!")
+    }
     call_sub <- function(call, old.name, new.name) {
       if(is.call(call)) {
         if(identical(call[[1L]], old.name)) call[[1L]] <- new.name
