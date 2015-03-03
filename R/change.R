6f187bdf7e2f32661edb011af17cc98cc45d614d
brodieG <brodieG@users.noreply.github.com>
2015-03-02 21:44:36 -0500

    show changes tweak

--- R/change.R
+++ R/change.R
@@ -25,10 +25,10 @@ setClass("unitizerChanges",
 
 setMethod("show", "unitizerChanges",
   function(object) {
-    if(object@failed[[1L]]) cat("- Replace", object@failed[[1L]] ,"out of", object@failed[[2L]], "failed tests\n")
-    if(object@new[[1L]]) cat("- Add", object@new[[1L]] , "out of", object@new[[2L]], "new tests\n")
-    if(object@removed[[1L]]) cat("- Remove", object@removed[[1L]], "out of", object@removed[[2L]], "removed tests\n")
-    if(object@corrupted[[1L]]) cat("- Replace", object@corrupted[[1L]], "out of", object@corrupted[[2L]], "tests with errors\n")
+    if(sum(object@failed)) cat("- Replace", object@failed[[1L]] ,"out of", object@failed[[2L]], "failed tests\n")
+    if(sum(object@new)) cat("- Add", object@new[[1L]] , "out of", object@new[[2L]], "new tests\n")
+    if(sum(object@removed)) cat("- Remove", object@removed[[1L]], "out of", object@removed[[2L]], "removed tests\n")
+    if(sum(object@corrupted)) cat("- Replace", object@corrupted[[1L]], "out of", object@corrupted[[2L]], "tests with errors\n")
     if(object@passed[[1L]]) cat("- Drop", object@passed[[1L]], "out of", object@passed[[2L]], "passed tests\n")
   }
 )
