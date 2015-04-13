1ed578968c54ad443b065c63a4ba66cddad741d5
brodieG <brodieG@users.noreply.github.com>
2015-04-13 19:29:06 -0400

    testup

--- tests/unitizer/translate.R
+++ tests/unitizer/translate.R
@@ -33,10 +33,10 @@ unitizer_sect("extract calls", {
 unitizer_sect("translate simple", {
   # NOTE: paths relative to R CMD check set-up
 
-  testthat_to_unitizer("blahbalh")  # not real file, should fail
-  testthat_to_unitizer("unitizer/helper/translate1.R", NULL)
-  testthat_to_unitizer("unitizer/helper/translate2.R", NULL)
-  testthat_to_unitizer(
+  testthat_translate_file("blahbalh")  # not real file, should fail
+  testthat_translate_file("unitizer/helper/translate1.R", NULL)
+  testthat_translate_file("unitizer/helper/translate2.R", NULL)
+  testthat_translate_file(
     "unitizer/helper/translate2.R", NULL, keep.testthat.call=TRUE
   ) # keep testthat call
 })
