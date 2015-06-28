4abad4be6c0820ae2e98b61c82d8ba80b724c1d5
brodieG <brodieG@users.noreply.github.com>
2015-06-27 21:19:35 -0400

    docup

--- R/unitizer.R
+++ R/unitizer.R
@@ -12,21 +12,23 @@ NULL
 #' Contains All The Data for Our Tests!
 #'
 #' Generally is populated through the \code{+} methods, with the exception of
-#' \code{items.ref}, which is added on creation.  I guess ideally all would be done
-#' through different \code{+} methods, but that would complicate the process a bit
-#' as that would require being able to distinguish between reference item lists and
-#' new item lists (and the latter should never really be added as that should happen
-#' item by item).  Maybe some day this will be cleaned up.
+#' \code{items.ref}, which is added on creation.  I guess ideally all would be
+#' done through different \code{+} methods, but that would complicate the
+#' process a bit as that would require being able to distinguish between
+#' reference item lists and new item lists (and the latter should never really
+#' be added as that should happen item by item).  Maybe some day this will be
+#' cleaned up.
 #'
 #' One of the challenges when maintaining this is the tension between wanting to
 #' keep all the item/test data in sub-objects, and the difficulty in extracting
 #' summary data across all items/tests in that structure.  As a result of this,
 #' a compromise solution has been to extract some of the relevant meta data
-#' into vectors/matrices available at the top level (e.g. the @@tests.* objects).
+#' into vectors/matrices available at the top level (e.g. the @@tests.*
+#' objects).
 #'
-#' Ultimately, we need far more specialized accessor functions that don't require
-#' understanding what those meta data mean exactly, and how they need to be used.
-#' An example is the \code{ignored} function.
+#' Ultimately, we need far more specialized accessor functions that don't
+#' require understanding what those meta data mean exactly, and how they need
+#' to be used.  An example is the \code{ignored} function.
 #'
 #' Things get particularly complicated with the \code{browse} objects, which
 #' basically rehash a lot of this data, but split into groups and sub-groups,
@@ -35,25 +37,30 @@ NULL
 #' \code{unitizer}.
 #'
 #' @keywords internal
-#' @slot id the identifier for the unitizer, typically a file name, but can be anything
+#' @slot id the identifier for the unitizer, typically a file name, but can be
+#'   anything
 #' @slot items.new a list of all the tests in the new file
 #' @slot items.ref a list of all the previously saved tests
 #' @slot items.new.map a vector that maps the entries in \code{items.new} to
 #'   those in \code{items.ref}, where position in vector is id/position in
 #'   slot \code{items.new}, and value is id/position in \code{items.ref}
 #'   new items will show up as NA here
-#' @slot items.new.calls.deparse a character vector of the deparsed calls in \code{items.new}
+#' @slot items.new.calls.deparse a character vector of the deparsed calls in
+#'   \code{items.new}
 #' @slot items.envs contains the environments for each call
 #' @slot tests.fail vector highlighting which tests failed
 #' @slot tests.new vector highlighting which tests did not exist in reference
-#' @slot test.status a vector that contains the result of the test ("pass", "fail", "new", "indeterminable")
+#' @slot test.status a vector that contains the result of the test ("pass",
+#'   "fail", "new", "indeterminable")
 #'   for every item in \code{items.new}
-#' @slot tests.result a logical matrix with a row for each item in \code{items.new} where each column
-#'   represents the result of each sub tests
-#' @slot tests.errorDetails an S4 object with a slot for each sub test, where the slot contains a
-#'   \code{\link{unitizerItemTestError-class}} object
-#'   either NULL or a character vector describing the test failure reason for every item in \code{items.new}
-#' @slot items.ref.calls.deparse like \code{items.new.calls.deparse}, but for the reference items
+#' @slot tests.result a logical matrix with a row for each item in
+#'   \code{items.new} where each column represents the result of each sub tests
+#' @slot tests.errorDetails an S4 object with a slot for each sub test, where
+#'   the slot contains a \code{\link{unitizerItemTestError-class}} object
+#'   either NULL or a character vector describing the test failure reason for
+#'   every item in \code{items.new}
+#' @slot items.ref.calls.deparse like \code{items.new.calls.deparse}, but for
+#'   the reference items
 #' @slot items.ref.map maps reference items to the new items; deleted items will
 #'   show up as NA here, where position in vector is id/position in slot
 #'   \code{items.ref}, and value is id/position in \code{items.new}
@@ -78,9 +85,6 @@ setClass(
     items.new.map="integer",
     items.new.calls.deparse="character",
     items.envs="list",
-
-    # NEED TO CLEAN THIS UP; SHOULD IT BE HANDLED BY METHODS? REALLY ANNOYING
-    # TO GET FAILED TESTS VS NEW TESTS VS. WHATEVER
 
     tests.fail="logical",                  # really need tests.fail?
     tests.error="logical",                 # really need tests.error? redundant with tests.result
