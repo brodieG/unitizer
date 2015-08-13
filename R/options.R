a3eb79233fdef251bb8216124f24cb1166c9befa
brodieG <brodieG@users.noreply.github.com>
2015-08-13 19:26:28 -0400

    drop 'setOptIfNotSet'

--- R/options.R
+++ R/options.R
@@ -2,27 +2,6 @@
 
 NULL
 
-#' Set Option if Not Set
-#'
-#' Utility function used by .onLoad to set a global option if it is not set
-#' already.  This is equivalent to conditionally using \code{option} to set
-#' options that are not defined yet.
-#'
-#' @export
-#' @param x the name of the option to set
-#' @param value the value to set the option to
-#' @return the result of calling \code{option} if option is not set, or NULL
-#'   invisibly if it is already set
-
-setOptIfNotSet <- function(x, value) {
-  if(!is.chr1(x)) stop("Argument `x` must be chracter(1L) and not NA")
-  if(!x %in% names(options())) {
-    opt.call <- list(quote(options))
-    opt.call[[x]] <- value
-    eval(as.call(opt.call), parent.frame())
-  } else invisible(NULL)
-}
-
 #' Global Option State Options
 #'
 #' Details on how to manage options state settings.
