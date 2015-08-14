98e9c5191759b3a351defec6447ff14ad9dec7b6
brodieG <brodieG@users.noreply.github.com>
2015-08-14 14:10:36 -0400

    alert about state management settings

--- R/onload.R
+++ R/onload.R
@@ -3,6 +3,13 @@
 NULL
 
 .onLoad <- function(libname, pkgname) {
+  if(is.null(getOption("unitizer.state")))
+    packageStartupMessage(
+      "`unitizer` state tracking defaults to \"",
+      .unitizer.opts.default[["unitizer.state"]], "\", use ",
+      "`option(unitizer.state=...)` to override; see `?unitizerState` ",
+      "for more details."
+    )
   options(
     .unitizer.opts.default[
       setdiff(names(.unitizer.opts.default), names(options()))
