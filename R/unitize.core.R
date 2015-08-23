brodieG/unitizer

commit 718dd1ca818d4e224db176eea8b5c59ef6034ef8

Fix #88: confusing 'unitize_dir' error message

R/unitize.core.R
@@ -192,8 +192,14 @@ unitize_core <- function(
     {
       reset_and_unshim(global)
       word_msg(
-        "Unexpectedly exited before storing unitizer; tests were not saved or",
-        "changed."
+        "Unexpectedly exited before storing unitizer; ",
+        if(length(unitizers) < 2L)
+          "tests were not saved or changed." else c(
+          "if you were reviewing a unitizer changes to that unitizer were not ",
+          "saved.  Note that any unitizers you *completed* review of should ",
+          "have been saved."
+        ),
+        sep=""
       )
     },
     add=TRUE

commit 97b183107f5aeff7fbe95a9b942b4f340fe2ac61

fix failure bug introduced in 718dd1c

R/unitize.core.R
@@ -193,7 +193,7 @@ unitize_core <- function(
       reset_and_unshim(global)
       word_msg(
         "Unexpectedly exited before storing unitizer; ",
-        if(length(unitizers) < 2L)
+        if(length(test.files) < 2L)
           "tests were not saved or changed." else c(
           "if you were reviewing a unitizer changes to that unitizer were not ",
           "saved.  Note that any unitizers you *completed* review of should ",