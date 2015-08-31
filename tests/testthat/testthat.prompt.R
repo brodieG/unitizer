681368fb2e45f9b4ae5afced34aba857da74e534
brodieG <brodieG@users.noreply.github.com>
2015-08-30 22:03:08 -0400

    testup

--- tests/testthat/testthat.prompt.R
+++ tests/testthat/testthat.prompt.R
@@ -44,19 +44,63 @@ test_that("faux prompt", {
     "> 1 \\+\n\\+ 1"
   )
   expect_identical(res, quote(1 + 1))
+  unitizer:::read_line_set_vals(c("(})"))
+  expect_error(
+    unitizer:::faux_prompt(prompt="> ", continue="+ "),
+    "Error in \"\\(\\}\\)\""
+  )
 })
 # Some of this needs to be done outside of testthat due to sinking
 
 unitizer:::read_line_set_vals(c("1 +", "1", "H", "Y"))
-txt <- capture.output(split=TRUE,
+txt <- capture.output(
   res <- unitizer:::unitizer_prompt(
       "hello", valid.opts=c(Y="[Y]es", N="[N]o")
+  )
+)
+unitizer:::read_line_set_vals(c("1 +", "1", "H", "Q"))
+txt2 <- capture.output(
+  res2 <- unitizer:::unitizer_prompt(
+    "hello", valid.opts=c(Y="[Y]es", N="[N]o"),
+    help="This is all the help you get"
   )
 )
 test_that("unitizer prompt", {
   expect_equal(
     txt,
     c("unitizer> 1 +", "+ 1", "[1] 2", "unitizer> H", "No help available.", "", "hello ([Y]es, [N]o, [Q]uit, [H]elp)?", "unitizer> Y")
+  )
+  expect_equal(
+    txt2,
+    c("unitizer> 1 +", "+ 1", "[1] 2", "unitizer> H", "This is all the help you get", "", "hello ([Y]es, [N]o, [Q]uit, [H]elp)?", "unitizer> Q")
+  )
+  expect_error(
+    unitizer:::unitizer_prompt(
+      "hello", valid.opts=c(Y="[Y]es", N="[N]o"), browse.env="not an env"
+    ),
+    "Argument `browse.env` must be an environment"
+  )
+  expect_identical(res2, "Q")
+  unitizer:::read_line_set_vals(character())
+  expect_error(
+    unitizer:::unitizer_prompt("hello", valid.opts=c(Y="[Y]es", N="[N]o")),
+    "Error: Logic Error: ran out of predifined readline input"
+  )
+  unitizer:::read_line_set_vals("1L")
+  expect_error(
+    unitizer:::unitizer_prompt(
+      "hello", valid.opts=c(Y="[Y]es", N="[N]o"),
+      exit.condition=unitizer:::exit_fun, valid.vals=2:3
+    ),
+    "Error : Logic Error: ran out of predifined readline input"
+  )
+  unitizer:::read_line_set_vals("2L")
+  expect_equal(
+    unitizer:::unitizer_prompt(
+      "hello", valid.opts=c(Y="[Y]es", N="[N]o"),
+      exit.condition=unitizer:::exit_fun, valid.vals=2:3
+    ),
+    2L
   )
 })
 
