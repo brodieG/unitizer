cfa70f129c0dfbe56728ef30a1d9cc6a6c973b24
brodieG <brodieG@users.noreply.github.com>
2015-09-23 12:21:06 -0400

    removing locale stuff since we abandonned effort

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,15 +1,3 @@
 library(testthat)
 
-old.ctype <- Sys.getlocale("LC_CTYPE")
-old.collate <- Sys.getlocale("LC_COLLATE")
-Sys.setlocale("LC_CTYPE", "en_US.UTF-8")
-Sys.setlocale("LC_COLLATE", "en_US.UTF-8")
-
-run_tests <- function() {
-  on.exit({
-    Sys.setlocale("LC_CTYPE", old.ctype)
-    Sys.setlocale("LC_COLLATE", old.collate)
-  } )
-  test_dir("testthat")
-}
-run_tests()
+test_dir("testthat")
