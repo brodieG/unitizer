0129cce793123a14c1449ec8995731ad4176ffa4
brodieG <brodieG@users.noreply.github.com>
2016-01-03 21:08:12 -0500

    fixes to 'diff_state'

--- R/state.compare.R
+++ R/state.compare.R
@@ -199,7 +199,9 @@ diff_state <- function(
     tar <- slot(target, i)
     if(is.null(cur) || identical(tar, cur)) next
 
-    word_cat(out <- sprintf("`%s` state mismatch:", i))
+    word_cat(header <- sprintf("`%s` state mismatch:", i))
+    out <- c(out, header)
+
     if(!is.int.1L(width) || width < 8L) width <- 8L else width <- width - 4L
 
     diff.string <- if(identical(i, "options")) {
@@ -233,17 +235,13 @@ diff_state <- function(
         diff.call[[2L]][[3L]] <- as.name(i)
         diff.call[[3L]] <- as.name(diff.name)
         diff.call.new <- diff.call.ref <- diff.call
-        diff.call.new[[2L]] <- as.name(".NEW")
-        diff.call.ref[[2L]] <- as.name(".REF")
-        diff.txt <- paste0(
-          "    ",
-          capture.output(
-            diff_obj_internal(
-              tar[[diff.name]], cur[[diff.name]],
-              tar.exp=diff.call.ref, cur.exp=diff.call.new, width=width
-        ) ) )
-        cat(diff.txt, sep="\n")
-        diff.txt
+        diff.call.new[[2L]][[2L]][[2L]] <- as.name(".NEW")
+        diff.call.ref[[2L]][[2L]][[2L]] <- as.name(".REF")
+        capture.output(
+          diff_obj_internal(
+            tar[[diff.name]], cur[[diff.name]],
+            tar.exp=diff.call.ref, cur.exp=diff.call.new, width=width
+        ) )
       } else if(deltas.count <= 10L) {
         # Depending on whether `all.equal` output is one or more lines, use
         # different display mode
@@ -298,21 +296,17 @@ diff_state <- function(
       diff.call <- quote(x$state@y)
       diff.call[[3L]] <- as.name(i)
       diff.call.new <- diff.call.ref <- diff.call
-      diff.call.new[[2L]] <- as.name(".NEW")
-      diff.call.ref[[2L]] <- as.name(".REF")
+      diff.call.new[[2L]][[2L]] <- as.name(".NEW")
+      diff.call.ref[[2L]][[2L]] <- as.name(".REF")
 
-      diff.txt <- paste0(
-        "    ",
-        capture.output(
-          diff_obj_internal(  # should try to collapse this with the one for options
-            tar, cur, tar.exp=diff.call.ref, cur.exp=diff.call.new, width=width
-          )
-        )
-      )
-      cat(diff.txt, sep="\n")
-      diff.txt
+      capture.output(
+        diff_obj_internal(  # should try to collapse this with the one for options
+          tar, cur, tar.exp=diff.call.ref, cur.exp=diff.call.new, width=width
+      ) )
     }
-    out <- c(out, diff.string)
+    diff.string.pad <- paste0("    ", diff.string)
+    word_cat(diff.string.pad, sep="\n")
+    out <- c(out, diff.string.pad)
   }
   msg.no.diff <- paste0(
     "Note that there may be state differences that are not reported here as ",
