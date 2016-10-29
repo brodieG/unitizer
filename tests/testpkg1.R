b156db6cbdd63980cd910df180e18f7c1a1d82a3
brodieG <brodieG@users.noreply.github.com>
2016-10-28 21:02:11 -0400

    cleanup interactive test

--- tests/testpkg1.R
+++ tests/testpkg1.R
@@ -14,40 +14,40 @@ if(interactive()) {
       setwd(old.dir)
       if("testpkg1" %in% search()) try(detach("package:testpkg1"))
       try(unloadNamespace("testpkg1"))
-      try(remove.packages("testpkg1", lib=lib.tmp))
+      try(remove.packages("testpkg1"))
       unlink(par.dir, recursive=TRUE)
     })
     dir.create(par.dir)
+
+    # old.dir should be package directory with test files
+
     old.dir <- setwd(par.dir)
     lib.tmp <- file.path(par.dir, "lib")
     test.tmp <- file.path(par.dir, "tests")
     pre.file <- file.path(test.tmp, "unitizer", "_pre")
     source.tests <-
       file.path(old.dir, "test_pkgs", "testpkg1", "testpkg1.0", "tests")
+
+    # copy files to new directory
+
     dir.create(lib.tmp)
     dir.create(test.tmp)
     file.copy(
       list.files(source.tests, full.names=TRUE), test.tmp, recursive=TRUE
     )
     dir.create(pre.file, recursive=TRUE)
-    cat(
-      sprintf(
-        "library(testpkg1, lib='%s')\n",
-        lib.tmp
-      ),
-      file=file.path(pre.file, "lib.R")
-    )
+    cat("library(testpkg1)\n", file=file.path(pre.file, "lib.R"))
     tp.0 <- file.path(old.dir, "test_pkgs", "testpkg1", "testpkg1.0")
-    install(tp.0, lib=.lib)
+    devtools::install(tp.0)
     unitize_dir(file.path(test.tmp, "unitizer"), auto.accept="new")
 
     # Upgrade the package, note we test against same store
 
     if("testpkg1" %in% search()) try(detach("package:testpkg1"))
     try(unloadNamespace("testpkg1"))
-    try(remove.packages("testpkg1", lib=lib.tmp))
+    try(remove.packages("testpkg1"))
     tp.1 <- file.path(old.dir, "test_pkgs", "testpkg1", "testpkg1.1")
-    install.packages(pkgs=tp.1, lib=lib.tmp, repos=NULL, type="src")
+    devtools::install(tp.1)
     unitize_dir(file.path(test.tmp, "unitizer"))
   })
 }
