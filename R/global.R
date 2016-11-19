864d2210628eadc4882d3b798d08a731ca99796f
brodieG <brodieG@users.noreply.github.com>
2016-11-19 16:07:33 -0500

    replace s4 for search path tracking with lists
    Was way too slow with S4

--- R/global.R
+++ R/global.R
@@ -214,8 +214,8 @@ setMethod(
   function(x, opts.ignore, ...) {
     stopifnot(is.character(opts.ignore))
     res <- new("unitizerGlobalTrackingStore")
-    res@search.path <- lapply(x@search.path, unitizerCompressTracking)
-    res@namespaces <- lapply(x@namespaces, unitizerCompressTracking)
+    res@search.path <- lapply(x@search.path, compress_search_data)
+    res@namespaces <- lapply(x@namespaces, compress_ns_data)
 
     # Don't store stuff with environments or stuff that is too big
     # (size cut-off should be an option?), or stuff that is part of the base or
@@ -256,25 +256,22 @@ search_as_envs <- function() {
   sp <- search()
   res <- setNames(lapply(seq_along(sp), as.environment), sp)
 
-  new("unitizerSearchData", .items=res, ns.dat=get_namespace_data())
+  list(search.path=res, ns.dat=get_namespace_data())
 }
 get_namespace_data <- function() {
-  new(
-    "unitizerNsListData",
-    .items=sapply(
-      loadedNamespaces(),
-      function(x) {
-        loc <- try(getNamespace(x)[[".__NAMESPACE__."]][["path"]])
-        ver <- try(getNamespaceVersion(x))
-        new(
-          "unitizerNamespaceData",
-          name=x,
-          lib.loc=if(!inherits(loc, "try-error")) loc else "",
-          version=if(!inherits(ver, "try-error")) ver else ""
-        )
-      },
-      simplify=FALSE
-  ) )
+  sapply(
+    loadedNamespaces(),
+    function(x) {
+      loc <- try(getNamespace(x)[[".__NAMESPACE__."]][["path"]])
+      ver <- try(getNamespaceVersion(x))
+      list(
+        name=x,
+        lib.loc=if(!inherits(loc, "try-error")) loc else "",
+        version=if(!inherits(ver, "try-error")) ver else ""
+      )
+    },
+    simplify=FALSE
+  )
 }
 #' @rdname global_structures
 #' @keywords internal
