5529e0880de76108afd3e64cdf919f2c7ae078a3
brodieG <brodieG@users.noreply.github.com>
2017-03-18 09:35:54 -0400

    capture shim messages on narrow width disp

--- R/shims.R
+++ R/shims.R
@@ -207,9 +207,9 @@ unitizerGlobal$methods(
           if(
             !identical(
               sprintf(
-                "Tracing function \"%s\" in package \"namespace:base\"\n", name
+                "Tracing function \"%s\" in package \"namespace:base\" ", name
               ),
-              conditionMessage(e)
+              gsub("\n", " ", conditionMessage(e))
             )
           ) {
             signalCondition(e)
@@ -255,9 +255,9 @@ unitizerGlobal$methods(
             if(
               !identical(
                 sprintf(
-                  "Untracing function \"%s\" in package \"namespace:base\"\n", i
+                  "Untracing function \"%s\" in package \"namespace:base\" ", i
                 ),
-                conditionMessage(e)
+                gsub("\n", " ", conditionMessage(e))
               )
             ) {
               signalCondition(e)
