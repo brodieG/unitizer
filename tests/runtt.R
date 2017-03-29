f546202203360f68d14651a494269beca4ff20a0
brodieG <brodieG@users.noreply.github.com>
2017-03-28 21:39:00 -0400

    restructure runtt again

--- tests/runtt.R
+++ tests/runtt.R
@@ -2,4 +2,4 @@ library(testthat)
 
 # used to be in here, but had to move to see error messages under travis
 
-local(source('testthat/helper/runtt.R', local=TRUE))
+source('testthat/helper/runtt.R', local=TRUE)
