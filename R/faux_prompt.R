8b46412c70bcb5614bd9ffa7e6a8c5be860eee33
brodieG <brodieG@users.noreply.github.com>
2017-11-11 13:47:23 -0500

    more newline fixes

--- R/faux_prompt.R
+++ R/faux_prompt.R
@@ -10,7 +10,7 @@ faux_prompt <- function(
 ) {
   res <- character()
   repeat {
-    res <- paste0(res, read_line(prompt), '\n')
+    res <- paste0(res, read_line(prompt), if(length(res)) '\n' else "")
     res.parse <- tryCatch(
       parsed <- parse(text=res),
       error=function(e) {
