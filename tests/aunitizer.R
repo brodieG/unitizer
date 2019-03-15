5023134014f0ce110e32caa0b5db9728dc3430ca
brodieG <brodieG@users.noreply.github.com>
2019-03-14 22:51:13 -0400

    rng ver for other tests

--- tests/aunitizer.R
+++ tests/aunitizer.R
@@ -1,8 +1,10 @@
 # Note, this assumes the WD is set to "tests" (i.e. as if run by R CMD check)
 
 library(unitizer)
-library(testthat)
+if(!require(testthat)) stop("testthat required for tests")
 
+RNGversion("3.5.2");
 unitize("unitizer/tests2.R")
 unitize("unitizer/sectionsRCMDCHECK.R")
 unitize("unitizer/translate.R")
+RNGversion(as.character(getRversion()))
