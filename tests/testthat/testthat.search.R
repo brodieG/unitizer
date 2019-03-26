8b2d786cae8a3dbed409818687aac10eab9a8ded
brodieG <brodieG@users.noreply.github.com>
2019-03-25 21:19:01 -0400

    tmp.lib fix

--- tests/testthat/testthat.search.R
+++ tests/testthat/testthat.search.R
@@ -26,8 +26,8 @@ local({
     unitizer:::.unitizer.global.settings.names
   )
   library(unitizer)
-  library(unitizerdummypkg1, lib.loc=getOption('unitizer.tmp.lib.loc'))
-  library(unitizerdummypkg2, lib.loc=getOption('unitizer.tmp.lib.loc'))
+  library(unitizerdummypkg1, lib.loc=tmp.lib)
+  library(unitizerdummypkg2, lib.loc=tmp.lib)
 
   test_that("Detecting packages", {
     expect_true(unitizer:::is.loaded_package("package:unitizer"))
