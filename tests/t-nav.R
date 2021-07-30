5e442dda0e6e01553f0d6d98bd6b592806620870
brodieG <brodieG@users.noreply.github.com>
2021-07-30 16:36:39 -0400

    more tests

---
+++ tests/t-nav.R
@@ -0,0 +1,22 @@
+source(file.path("_helper", "init.R"))
+nav <- file.path("_helper", "unitizers", "nav.R")
+
+# Simple navigation tests that don't require complex unitizers
+
+# - "Re-run bookmark" -----=----------------------------------------------------
+
+# Relates to #278.  Tests both Review and Browse
+
+unitize(nav, interactive.mode=TRUE)
+R
+Y
+B
+7
+R
+Y
+B
+9
+R
+Y
+Q
+
