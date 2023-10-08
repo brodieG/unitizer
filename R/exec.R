31f5580b743bf3fbde63bc1f1a9df88643ede564
brodieG <brodieG@users.noreply.github.com>
2023-10-07 21:11:22 -0400

    update copyright notices...
    Switch to only keep the year in the COPYING file

--- R/exec.R
+++ R/exec.R
@@ -1,6 +1,6 @@
-# Copyright (C) 2022 Brodie Gaslam
+# Copyright (C) Brodie Gaslam
 #
-# This file is part of "unitizer"
+# This file is part of "unitizer - Interactive R Unit Tests"
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
