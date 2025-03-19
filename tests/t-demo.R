418d3b834094e5ee616cf14ca8dca8b4e0365f60
brodieG <brodieG@users.noreply.github.com>
2025-03-19 13:08:33 -0400

    restore original demo tests

--- tests/t-demo.R
+++ tests/t-demo.R
@@ -58,4 +58,84 @@ unitizer:::update_fastlm(".", version = "0.1.0")
 inst_pak(".")
 unitizer:::read_line_set_vals(c("Y", "Y", "Y", "Y", "Y"))
 untz <- unitize(FLM.TEST.FILE, interactive.mode = TRUE)
-untz
+is(untz, "unitizer_result")
+print(untz)
+# Re-running doesn't change unitizer
+untz2 <- unitize(FLM.TEST.FILE, interactive.mode = TRUE)
+print(untz2)
+# Rejecting failed tests does not change unitizer
+update_fastlm(".", version = "0.1.1")
+inst_pak(".")
+
+unitizer:::read_line_set_vals(c("N", "N", "Y"))
+unitizer:::capture_output(
+  untz3 <- unitize(FLM.TEST.FILE, interactive.mode = TRUE)
+)
+print(untz3)
+
+# - "demo review" --------------------------------------------------------------
+
+# review is always in interactive mode
+unitizer:::read_line_set_vals(c("5", "Q"))
+review(FLM.TEST.STORE)
+
+# - "use.diff" -----------------------------------------------------------------
+
+# Use this opportunity to make sure `use.diff=FALSE` works as intended
+unitizer:::read_line_set_vals("Q")
+unitize(FLM.TEST.FILE, interactive.mode = TRUE, use.diff = FALSE)
+unitizer:::read_line_set_vals(c(".DIFF$state", "Q"))
+
+unitize(FLM.TEST.FILE, interactive.mode = TRUE, use.diff = FALSE)
+
+# - "failing diff" -------------------------------------------------------------
+
+unitizer:::read_line_set_vals("Q")
+mock(diffobj::diffObj, quote(stop("A failing diff.")))
+unitize(FLM.TEST.FILE, interactive.mode = TRUE)
+unmock(diffobj::diffObj)
+
+# - "multi-accept" -------------------------------------------------------------
+
+# Test what happens if we back out of a multi-accept
+
+unitizer:::read_line_set_vals(c("YY", "N", "Q"))
+unitize(FLM.TEST.FILE, interactive.mode = TRUE)
+
+# - "multi-input" --------------------------------------------------------------
+
+# Or if we request to go to unreviewed when there are none
+unitizer:::read_line_set_vals(c("YY", "Y", "B", "U", "Q"))
+unitize(FLM.TEST.FILE, interactive.mode = TRUE)
+
+# - "warn in parse" ------------------------------------------------------------
+
+# Make sure parse warnings are issued
+unitizer:::read_line_set_vals(c("-2147483648L", "Q"))
+txt8 <- unitizer:::capture_output(unitize(FLM.TEST.FILE,
+    interactive.mode = TRUE))
+
+any(grepl("qualified with L", txt8$message))
+
+# - "demo changes" -------------------------------------------------------------
+#
+# Now actually accept the changes
+unitizer:::read_line_set_vals(c("Y", "Y", "Y"))
+untz5 <- unitize(FLM.TEST.FILE, interactive.mode = TRUE)
+unitizer:::read_line_set_vals(NULL)
+
+# - "get_package_dir" ----------------------------------------------------------
+
+# These were in t-get but we moved them here to avoid re-loading pkgs.R
+
+unitizer:::get_package_dir(f)  # empty
+test.dir.1 <- file.path(".", "utzflm.Rcheck", "utzflm", "R")
+identical(
+  unitizer:::get_package_dir(test.dir.1),
+  normalizePath(dirname(test.dir.1), winslash = "/")
+)
+test.dir.2 <- file.path(".", "utzflm.Rcheck")
+identical(
+  unitizer:::get_package_dir(file.path(test.dir.2, "tests", "tests.R")),
+  normalizePath(file.path(test.dir.2, "utzflm"), winslash = "/")
+)
