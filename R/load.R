845674800a579b9e491c5baeb2556fd1eae2b914
brodieG <brodieG@users.noreply.github.com>
2025-03-19 14:54:07 -0400

    disable part of the instrumentation

--- R/load.R
+++ R/load.R
@@ -224,7 +224,7 @@ store_unitizer <- function(unitizer) {
   global.par.env <- unitizer@global$par.env
   old.global.par.env.par <- parent.env(global.par.env)
   on.exit(parent.env(global.par.env) <- old.global.par.env.par, add=TRUE)
-  if(getOption('unitizer.store.dump.envs', FALSE)) dump_env_ancestry(unitizer)
+  # if(getOption('unitizer.store.dump.envs', FALSE)) dump_env_ancestry(unitizer)
   parent.env(global.par.env) <- baseenv()
 
   # zero out connections we'v been using
