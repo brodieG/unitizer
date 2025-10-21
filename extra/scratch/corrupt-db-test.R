0e548e1c70fb61087c3539ba85a0f27bfb51e2ff
brodieG <brodieG@users.noreply.github.com>
2025-10-21 10:08:47 -0400

    reduced failing example

--- extra/scratch/corrupt-db-test.R
+++ extra/scratch/corrupt-db-test.R
@@ -1,65 +1,53 @@
+# source("../extra/scratch/corrupt-db-test.R", echo=TRUE)
 # Overflow tests from testthat.unitize.R
 
 source(file.path("_helper", "init.R"))
 source(file.path("_helper", "pkgs.R"))
 
+x <- 1:10
+y <- x ^ 3
+
 library(unitizer)
 
-# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-# - "unreviewed variations" ----------------------------------------------------
-# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+# library(utzflm, lib.loc=getOption('unitizer.tmp.lib.loc'))
+# detach('package:utzflm', unload=TRUE)
 
-# Test unreviewed
-# Accept one and go to unreviewed
-# Accept one more and browse and go to unreviewed
-# Accept two remaining and confirm no unreviewed
-# No unreviewed tests
-unitizer:::read_line_set_vals(
-  c("Y", "Q", "U", "Y", "B", "U", "Y", "Y", "U", "B", "U", "Q")
-)
-out <- unitizer:::capture_output(unitize(FLM.TEST.FILE, interactive.mode = TRUE))
-unitizer:::clean_eval_exp(out)
-
-# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-# - "Re-eval" ------------------------------------------------------------------
-# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-
-# Test re-eval
-# Re-eval and jump back to file 1
-# Quit from file 1 and back to main menu
-# Accept one test in file 2 and quit
-# Go to file 3, accept one, and Re-eval all
-
-unitizer:::read_line_set_vals(
-  c("1", "Y", "R", "Y", "Q", "2", "Y", "Y", "3", "Y", "RR", "Y", "Q", "Q")
-)
-untz1 <- unitize_dir(FLM.TEST.DIR, interactive.mode = TRUE)
-print(untz1)
-# remove temp file names and display
-invisible(lapply(untz1, function(x) {print(x); cat('\n')}))
-
-# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-# - "Section Extra" ------------------------------------------------------------
-# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-
-# Make sure that deleted items from a section are still marked from that
-# section upgrade to version two to use the files that are set up for that
-# there; notice update_fastlm_*extra*
-# Re-set by dropping unitizers
+library(utzflm, lib.loc=TMP.LIB)
+fastlm(x, y)
 
 unlink(list.dirs(FLM.TEST.DIR, recursive = FALSE), recursive = TRUE)
-unitizer:::update_fastlm_extra(FLM)
-inst_pak(FLM)
-test.file.1 <- file.path(FLM.TEST.DIR, "unitizer.fastlm.R")
-test.file.2 <- file.path(FLM.TEST.DIR, "unitizer.fastlm2.R")
-test.store <- file.path(FLM.TEST.DIR, "store2.unitizer")
-# First auto accept all initial tests, and then re-run with second version to
-# make sure deleted tests are where we think they should be
-out.1 <-
-  unitizer:::capture_output(unitize(test.file.1, test.store, auto.accept = "new"))
-unitizer:::read_line_set_vals(c("B", "Q"))
-out.2 <- unitizer:::capture_output(
-  untz.2 <- unitize(test.file.2, test.store, interactive.mode = TRUE)
-)
-attributes(untz.2) <- NULL
-untz.2
+{
+  dir <- FLM
+  unitizer:::check_test_dir(dir)
+  detach("package:utzflm", unload=TRUE)
+  lm.dir <- "flm1"
+  untz.dir <- system.file(package="unitizer")
+  lm.dir.full <- file.path(untz.dir, "expkg", lm.dir)
+  cpy.files <- unitizer:::.test.core.files
+  cpy.from <- file.path(lm.dir.full, cpy.files)
+  cpy.to <- file.path(dir, cpy.files)
+
+  invisible(file.copy(cpy.from, cpy.to, overwrite=TRUE))
+}
+install.packages(FLM, repos=NULL, type='src', lib=TMP.LIB)
+
+# lazyLoadDBfetch(ekey, datafile, compressed, envhook)
+
+
+library(utzflm, lib.loc=TMP.LIB)
+fastlm
+
+
+# test.file.1 <- file.path(FLM.TEST.DIR, "unitizer.fastlm.R")
+# test.file.2 <- file.path(FLM.TEST.DIR, "unitizer.fastlm2.R")
+# test.store <- file.path(FLM.TEST.DIR, "store2.unitizer")
+# # First auto accept all initial tests, and then re-run with second version to
+# # make sure deleted tests are where we think they should be
+# out.1 <-
+#   unitizer:::capture_output(unitize(test.file.1, test.store, auto.accept = "new"))
+# unitizer:::read_line_set_vals(c("B", "Q"))
+# out.2 <- unitizer:::capture_output(
+#   untz.2 <- unitize(test.file.2, test.store, interactive.mode = TRUE)
+# )
+# attributes(untz.2) <- NULL
+# untz.2
